a26bb4148adb69b0ab5179261db067718e8d95a4
priyankaarora07 <62415388+priyankaarora07@users.noreply.github.com>
2020-03-27 23:50:35 +0530

    Upload project

---
+++ run.bat
@@ -0,0 +1,17 @@
+
+@echo
+<<<<<<< HEAD
+
+cd F:\Work\Workspace\QBCS_Gulf\src\FunctionalComponents
+javac Demo.java
+cd..
+java FunctionalComponents.Demo
+=======
+cd F:\Work\Workspace\Git_Demo\src\FunctionalComponents
+javac Demo.java
+cd..
+java FunctionalComponents.Demo
+
+PAUSE
+
+>>>>>>> 905ca433fdfe553ae3e8710524550513d52d9e1a
